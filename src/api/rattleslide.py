05cc10d2295b0f8ab5e4f172212956a685b8edf9
celia <celia>
2023-11-14 14:02:50 +0000

    feat: start of the API and tests #15

--- src/api/rattleslide.py
+++ src/api/rattleslide.py
@@ -1,19 +1,96 @@
 import j2l.pytactx.agent as pytactx
 
-class Snake:
+class ISnake:
 
 	GAUCHE = -1
 	DROITE = 1
-	
-	def __init__(self, id, arena, username, password, server):
-		self.__agent = pytactx.Agent(id, arena, username, password, server)
 
-	def spawnSerpent():
-		pass
-		
-	def lookAt(direction):
-		#ne peux pas lookAt dans la direction de son corps
-		pass
+	#Accesseurs
+	def getCoordinates(self) -> tuple[int, int]:
+		"""
+		Return the coordinates x and y of the head of your snake
+		"""
 
-	def getSegmentsPositions():
-		pass
+	def getDirection(self) -> int:
+		"""
+	    Return the direction your snake is looking at:
+		 		0: Est
+        1: South
+        2: West
+        3: North
+		"""
+
+	def getLength(self)-> int:
+		"""
+		Return the length of your snake
+		"""
+
+	def getFoodCoordinates(self) -> tuple[tuple[int,int]]:
+		"""
+			Return the x and y of every food present on the map
+		"""
+
+	def getPlayersHeads(self) -> tuple[tuple[int, int, int]]:
+		"""
+		 Return x and y of every players 'snakes' head on the map. 
+		 Also return the length of each snakes.
+		"""
+
+	def getSegmentsPositions(self) -> tuple[tuple[int,int]]:
+		"""
+		 Return x and y of every segments of your own snake on the map.
+		"""
+
+#	def getRange(self) -> dict: 
+
+	def update(self) -> None:
+		"""
+		Fetch the last values of player data from server and send all pending requests to the server.
+		"""
+
+	def lookAt(self, direction: int) -> None:
+		"""
+			Change the direction your snake is looking at.
+	 		You can only turn 90° from your current direction at a time :
+				-1 : to the left
+				1 : to the right
+		"""
+
+class Snake(ISnake):
+
+	def __init__(self, id, arena, username, password, server, port):
+		self.__agent = pytactx.Agent(id, arena, username, password, server, port)
+		self.__length = 4
+
+	def getCoordinates(self):
+		return (self.__agent.x, self.__agent.y)
+
+	def getDirection(self):
+		return self.__agent.dir
+
+	def getLength(self):
+		return self.__length
+
+def getFoodCoordinates(self):
+	res = []
+	map = self.__agent.map
+	for i in range (0, len(map)):
+		for j in range (0, len(map[0])):
+			if(map[i][j] == 2):
+				res.append((i,j))
+	return res
+
+def __countlength(id):
+	#used to count the length of the given snake (do not forget the head (id -1))
+	pass
+
+def getPlayersHeads(self):
+	res = []
+	map = self.__agent.map
+	for i in range (0, len(map)):
+		for j in range (0, len(map[0])):
+			#if is the head of the player but isn't the texture for a wall (1)
+			if(map[i][j] %2 == 1 and map[i][j] > 1): 
+				length = __countlength(map[i][j] + 1)
+				res.append([i,j, length]) #didn't registered the length of the snake yet
+	return res
--- tests/api/test_rattleslide.py
+++ tests/api/test_rattleslide.py
@@ -1,22 +1,29 @@
+from src.api.rattleslide import Snake
+
 import src.api.rattleslide as rattle
 '''construction d/'un serpent'''
-snake = rattle.Snake(id="demo", arena="rattleslide ", username="TEST", password="demo", server="mqtt.jusdel")
+snake = rattle.Snake(id="demo",
+                     arena="rattleslide ",
+                     username="TEST",
+                     password="demo",
+                     server="mqtt.jusdel")
+
 
 def test_spawnSerpent():
-	'''test de la fonction spawnSerpent'''
-	pass
+  '''test si un serpent apparait bien dans les limites de la carte
+  '''
+  x, y = snake.getCoordinates()
+  assert 0 <= x < 30 and 0 <= y < 30, f"le serpent n'apparait pas dans les limites de la carte x={x} et y={y}"
 
-def test_lookAt ():
-	'''test de la fonction lookAt'''
 
-	#cas de test 1
-	
-	
-	
-	#cas de test 2
+def test_lookAt():
+  '''test de la fonction lookAt'''
+
+  #cas de test 2
+
 
 def test_getSegmentsPositions():
-	'''test de la fonction getSegmentsPositions'''
-	#cas de test 1
-	
-	#cas de test 2
+  '''test de la fonction getSegmentsPositions'''
+  #cas de test 1
+
+  #cas de test 2
