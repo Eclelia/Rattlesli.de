9ac1b817710699f235d06c4f07b72d1bb5a347a0
bob <bob>
2023-11-14 09:00:52 +0000

    draft: prototype des fonctions tests #15

--- tests/api/test_rattleslide.py
+++ tests/api/test_rattleslide.py
@@ -2,8 +2,21 @@ import src.api.rattleslide as rattle
 '''construction d/'un serpent'''
 snake = rattle.Snake(id="demo", arena="rattleslide ", username="TEST", password="demo", server="mqtt.jusdel")
 
+def test_spawnSerpent():
+	'''test de la fonction spawnSerpent'''
+	pass
+
 def test_lookAt ():
 	'''test de la fonction lookAt'''
+
+	#cas de test 1
+	
+	
+	
+	#cas de test 2
+
+def test_getSegmentsPositions():
+	'''test de la fonction getSegmentsPositions'''
 	#cas de test 1
 	
 	#cas de test 2
