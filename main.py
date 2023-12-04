ee1216088cf4e01e8714636b26121ee0b43edae5
lecrois211 <123160576+lecrois211@users.noreply.github.com>
2023-12-04 12:10:36 +0000

    finished work on arbitre
    added functions addFood, setTileAsFood, destroyFruit, resetArenaFruits.
    
    Function AddFoodV2 can be implemented as a nice to have.
    
    Code is documented

--- main.py
+++ main.py
@@ -1,22 +1,113 @@
 import j2l.pytactx.agent as pytactx
+import time
 import random
 
 arbitre = pytactx.Agent(playerId=input("👾 id: "),
-						arena="rattleslide",
-						username="demo",
-						password="demo",
-						server="mqtt.jusdeliens.com",
-						verbosity=2)
+                        arena="rattleslide",
+                        username="demo",
+                        password="demo",
+                        server="mqtt.jusdeliens.com",
+                        verbosity=2)
+
+arbitre.ruleArena("map", 1)
+arbitre.ruleArena("mapImgs", [1, "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3c77f9db-8565-47a2-b216-955e5b9840e9/dbpkj28-ff95a573-79e1-41bf-9540-396807ce7204.png/v1/fill/w_400,h_566/pixel_pumpkin_by_tanmo_dbpkj28-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzNjNzdmOWRiLTg1NjUtNDdhMi1iMjE2LTk1NWU1Yjk4NDBlOVwvZGJwa2oyOC1mZjk1YTU3My03OWUxLTQxYmYtOTU0MC0zOTY4MDdjZTcyMDQucG5nIiwiaGVpZ2h0IjoiPD01NjYiLCJ3aWR0aCI6Ijw9NDAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLndhdGVybWFyayJdLCJ3bWsiOnsicGF0aCI6Ilwvd21cLzNjNzdmOWRiLTg1NjUtNDdhMi1iMjE2LTk1NWU1Yjk4NDBlOVwvdGFubW8tNC5wbmciLCJvcGFjaXR5Ijo5NSwicHJvcG9ydGlvbnMiOjAuNDUsImdyYXZpdHkiOiJjZW50ZXIifX0.IgQBFaw1ySPYQBMiAB9mBgClLCznFzmCODo3LxR-Syk"])
 
 arbitre.fire(False)
 
-def AddFood(number_food_chunk):
-	for i in range (number_food_chunk):
-		spawn_height = random.randint(0, arbitre.gridRows)
-		spawn_width = random.randint(0, arbitre.gridColumns)
-		arbitre.ruleArena()
+def AddFoodV2(probabilite):
+	...
 
+
+def AddFood(number_food_chunk):
+	
+	"""
+	Function AddFood
+
+ 	Create a number_food_chunk fruits on the arena
+
+	Arguments :
+		
+	number_food_chunk - Number of chunk of food to create, must be int
+
+	"""
+	
+	resetArenaFruits()
+	arbitre.update()
+	map = arbitre.map
+	for i in range(number_food_chunk):
+		x = random.randint(0, len(map[0])-1)
+		y = random.randint(0,len(map)-1)
+		print(f"x = {x} \n y = {y}")
+		if map[y][x] in [0,1] :
+			map[y][x] = 1
+	arbitre.ruleArena('map', map)
+	arbitre.update()
+
+
+
+def setTileAsFood(x, y):
+	"""
+	Function setTileAsFood
+
+ 	Create a fruit at x, y coordinateq
+
+	Arguments :
+ 
+	x - Coordinate x of new fruit to create, must be int
+ 
+ 	y - Coordinate y of new fruit to create, must be int
+
+	"""
+	
+	arbitre.update()
+	map = arbitre.map
+	if x < len(map[0]) and y < len(map):
+		if map[y][x] == 0 :
+			map[y][x] = 1
+	arbitre.ruleArena('map', map)
+	arbitre.update()
+
+def destroyFruit(x, y):
+	"""
+	Function destroyFruit
+
+ 	Destroy fruit at x, y coordinates
+
+	Arguments :
+
+	x - Coordinate x of fruit to destroy, must be int
+
+	y - Coordinate y of fruit to destroy, must be int
+
+	"""
+	
+	arbitre.update()
+	map = arbitre.map
+	if x < len(map[0]) and y < len(map):
+		if map[y][x] == 1 :
+			map[y][x] = 0
+	arbitre.ruleArena('map', map)
+	arbitre.update()
+
+def resetArenaFruits():
+	"""
+	Function resetArenaFruits
+
+ 	Destroy all fruits on the arena
+
+	"""
+	
+	arbitre.update()
+	map = arbitre.map
+	for y in range(len(map)):
+		for x in range(len(map[0])):
+			if map[y][x] == 1:
+				map[y][x] = 0
+	arbitre.ruleArena('map', map)
+	arbitre.update()
+
+resetArenaFruits()
 
 while True:
-	arbitre.update()
-	arbitre.lookAt((arbitre.dir + 1) % 4)
+	AddFood(25)
+	time.sleep(1)
